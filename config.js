fd131a49ef6338a3eaeb4e51f9dacbd16ca3e724
David32094 <elprincipe32094@gmail.com>
2025-11-24 23:04:13 -0300

    Actualizar URL Cloudflare: ultra-tells-construct-elder

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // CONFIGURACIÓN - EDITA SOLO LA URL DE ABAJO
 var API_CONFIG = {
-    API_URL: 'https://ddr-breeds-pushing-mystery.trycloudflare.com/api/send-command'
+    API_URL: 'https://ultra-tells-construct-elder.trycloudflare.com/api/send-command'
 };
