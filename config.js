dd35656cb63c845ae26a76d71df09c69df17265e
David32094 <elprincipe32094@gmail.com>
2025-11-24 22:34:59 -0300

    Auto-update: URL Cloudflare https://runner-juan-invite-trigger.trycloudflare.com

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // CONFIGURACIÓN - EDITA SOLO LA URL DE ABAJO
 var API_CONFIG = {
-    API_URL: 'https://manufacturing-rent-incentive-oil.trycloudflare.com/api/send-command'
+    API_URL: 'https://runner-juan-invite-trigger.trycloudflare.com/api/send-command'
 };
 
