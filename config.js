898ea646d940374e37316c0385a9d9bcf04e56b3
David32094 <elprincipe32094@gmail.com>
2025-11-24 22:41:21 -0300

    Auto-update: URL Cloudflare https://swim-fees-occupation-displayed.trycloudflare.com

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // CONFIGURACIÓN - EDITA SOLO LA URL DE ABAJO
 var API_CONFIG = {
-    API_URL: 'https://karaoke-steal-decor-belle.trycloudflare.com/api/send-command'
+    API_URL: 'https://swim-fees-occupation-displayed.trycloudflare.com/api/send-command'
 };
 
