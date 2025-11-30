bab191a144a0028cf49e3535d87160fcbd10ee0e
David32094 <elprincipe32094@gmail.com>
2025-11-29 23:23:10 -0300

    Update emotes-data.js

--- emotes-data.js
+++ emotes-data.js
@@ -91,20 +91,185 @@ const EMOTES_DATA = {
             Number: "17",
             Id: "909049010",  // ID real del P90 según el bot
             Image: "emotes/evolutivas/evolutiva 17.png"
+        },
+        {
+            Number: "146",
+            Id: "909033002",  // MP5 evolutiva
+            Image: "emotes/evolutivas/evolutiva 146.png"
+        },
+        {
+            Number: "211",
+            Id: "909038004",  // Puño evolutivos
+            Image: "emotes/evolutivas/evolutiva 211.png"
         }
         // Agregar más emotes evolutivas aquí si es necesario
     ],
     
     normales: [
         // Emotes normales - Usar números de emotes.json
-        // Ejemplo: si el emote normal tiene Number "10" en emotes.json
         {
-            Number: "10",  // Número del emote en emotes.json
-            Id: "909000010",  // ID del emote en emotes.json
-            Image: "emotes/normales/normal-10.png"  // Ruta: Panel David IA/emotes/normales/normal-10.png
+            Number: "1",
+            Id: "909000001",
+            Image: "emotes/normales/normal-1.png"
+        },
+        {
+            Number: "2",
+            Id: "909000002",  // Risa
+            Image: "emotes/normales/normal-2.png"
+        },
+        {
+            Number: "3",
+            Id: "909000003",
+            Image: "emotes/normales/normal-3.png"
+        },
+        {
+            Number: "4",
+            Id: "909000004",
+            Image: "emotes/normales/normal-4.png"
+        },
+        {
+            Number: "5",
+            Id: "909000005",
+            Image: "emotes/normales/normal-5.png"
+        },
+        {
+            Number: "6",
+            Id: "909000006",
+            Image: "emotes/normales/normal-6.png"
+        },
+        {
+            Number: "7",
+            Id: "909000007",  // Pio Piu
+            Image: "emotes/normales/normal-7.png"
+        },
+        {
+            Number: "8",
+            Id: "909000008",
+            Image: "emotes/normales/normal-8.png"
+        },
+        {
+            Number: "9",
+            Id: "909000009",
+            Image: "emotes/normales/normal-9.png"
+        },
+        {
+            Number: "10",
+            Id: "909000010",
+            Image: "emotes/normales/normal-10.png"
+        },
+        {
+            Number: "11",
+            Id: "909000011",
+            Image: "emotes/normales/normal-11.png"
+        },
+        {
+            Number: "12",
+            Id: "909000012",  // Lagartija
+            Image: "emotes/normales/normal-12.png"
+        },
+        {
+            Number: "13",
+            Id: "909000013",
+            Image: "emotes/normales/normal-13.png"
+        },
+        {
+            Number: "14",
+            Id: "909000014",
+            Image: "emotes/normales/normal-14.png"
+        },
+        {
+            Number: "15",
+            Id: "909000015",
+            Image: "emotes/normales/normal-15.png"
+        },
+        {
+            Number: "16",
+            Id: "909000016",
+            Image: "emotes/normales/normal-16.png"
+        },
+        {
+            Number: "17",
+            Id: "909000017",
+            Image: "emotes/normales/normal-17.png"
+        },
+        {
+            Number: "18",
+            Id: "909000018",
+            Image: "emotes/normales/normal-18.png"
+        },
+        {
+            Number: "19",
+            Id: "909000019",
+            Image: "emotes/normales/normal-19.png"
+        },
+        {
+            Number: "20",
+            Id: "909000020",  // Paso del mal
+            Image: "emotes/normales/normal-20.png"
+        },
+        {
+            Number: "33",
+            Id: "909000034",  // Bandera marrón
+            Image: "emotes/normales/normal-33.png"
+        },
+        {
+            Number: "75",
+            Id: "909000077",  // Bailesito
+            Image: "emotes/normales/normal-75.png"
+        },
+        {
+            Number: "93",
+            Id: "909000095",  // Pingüinito
+            Image: "emotes/normales/normal-93.png"
+        },
+        {
+            Number: "133",
+            Id: "909000135",  // Piedra, papel o tijera
+            Image: "emotes/normales/normal-133.png"
+        },
+        {
+            Number: "140",
+            Id: "909000142",  // Mueve la cola como Anitta
+            Image: "emotes/normales/normal-140.png"
+        },
+        {
+            Number: "168",
+            Id: "909034014",  // Ajolote moviéndolo
+            Image: "emotes/normales/normal-168.png"
+        },
+        {
+            Number: "190",
+            Id: "909036008",  // Sin nombre
+            Image: "emotes/normales/normal-190.png"
+        },
+        {
+            Number: "196",
+            Id: "909037001",  // Venado
+            Image: "emotes/normales/normal-196.png"
+        },
+        {
+            Number: "206",
+            Id: "909037011",  // Sin nombre
+            Image: "emotes/normales/normal-206.png"
+        },
+        {
+            Number: "208",
+            Id: "909038001",  // Sin nombre
+            Image: "emotes/normales/normal-208.png"
+        },
+        {
+            Number: "268",
+            Id: "909042007",  // Sin nombre
+            Image: "emotes/normales/normal-268.png"
+        },
+        {
+            Number: "291",
+            Id: "909044004",  // Oveja
+            Image: "emotes/normales/normal-291.png"
         }
         // Agregar más emotes normales aquí según emotes.json
         // Formato: Number debe coincidir con el "Number" en emotes.json
+        // Las imágenes deben estar en: Panel David IA/emotes/normales/normal-{Number}.png
     ],
     
     duo: [
@@ -131,4 +296,5 @@ const EMOTES_DATA = {
 // 2. Agregar las rutas correctas a las imágenes
 // 3. Asegurarse de que Number e Id coincidan con los datos del juego
 // 4. Mantener el formato JSON válido
+
 
