307a6e88c71ba04f0dc9aad05318b463ebc8fe9e
David32094 <elprincipe32094@gmail.com>
2025-11-29 23:59:12 -0300

    Update weapon-names.js

--- weapon-names.js
+++ weapon-names.js
@@ -5,25 +5,24 @@
 // Basado en el mapeo del bot: evo_emotes
 
 const WEAPON_NAMES = {
-    // Evolutivas (1-17) - Mapeo exacto del bot
+    // Evolutivas (1-17, 146, 211) - Mapeo exacto del bot
+    // Nota: Los números 2, 7, 12 son normales, no evolutivas
     "1": "AK47",           // 909000063
-    "2": "SCAR",           // 909000068
     "3": "MP40",           // 909000075 (1st MP40)
     "4": "MP40",           // 909040010 (2nd MP40)
     "5": "M1014",          // 909000081 (1st M1014)
     "6": "M1014",          // 909039011 (2nd M1014)
-    "7": "XM8",            // 909000085
     "8": "FAMAS",          // 909000090
     "9": "UMP",            // 909000098
     "10": "M1887",         // 909035007
     "11": "Woodpecker",    // 909042008
-    "12": "Groza",         // 909041005
     "13": "M4A1",          // 909033001
     "14": "Thompson",      // 909038010
     "15": "G18",           // 909038012
     "16": "Parafal",       // 909045001
     "17": "P90",           // 909049010
     "146": "MP5 evolutiva", // 909033002
+    "180": "AN94",         // 909035012
     "211": "Puño evolutivos", // 909038004
     
     // Emotes normales
